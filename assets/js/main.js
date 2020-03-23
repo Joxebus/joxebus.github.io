88dbf84e8c7222399a00d461b68cbe96ec34a5fe
Omar Bautista <joxebus@gmail.com>
2020-03-22 23:09:10 -0600

    Update index.html with medium blog posts

--- assets/js/main.js
+++ assets/js/main.js
@@ -62,6 +62,40 @@ jQuery(document).ready(function($) {
         }
     );
 
+    $("#rss-feeds-medium").rss(
+
+        //Change this to your own rss feeds
+        "https://medium.com/feed/@obautista",
+
+        {
+        // how many entries do you want?
+        // default: 4
+        // valid values: any integer
+        limit: 3,
+
+        // the effect, which is used to let the entries appear
+        // default: 'show'
+        // valid values: 'show', 'slide', 'slideFast', 'slideSynced', 'slideFastSynced'
+        effect: 'slideFastSynced',
+
+        // will request the API via https
+	    // default: false
+	    // valid values: false, true
+	    ssl: true,
+
+        // outer template for the html transformation
+        // default: "<ul>{entries}</ul>"
+        // valid values: any string
+        layoutTemplate: "<div class='items'>{entries}</div>",
+
+        // inner template for each entry
+        // default: '<li><a href="{url}">[{author}@{date}] {title}</a><br/>{shortBodyPlain}</li>'
+        // valid values: any string
+        entryTemplate: '<div class="item"><h3 class="title"><a href="{url}" target="_blank">{title}</a></h3><div><p>{shortBodyPlain}</p><a class="more-link" href="{url}" target="_blank"><i class="fas fa-external-link-alt"></i>Read more</a></div></div>'
+
+        }
+    );
+
     $("#rss-feeds-youtube").rss(
 
         //Change this to your own rss feeds
@@ -95,7 +129,7 @@ jQuery(document).ready(function($) {
 
         }
     );
-    
+
     /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
     new GitHubCalendar("#github-graph", "Joxebus");
     
