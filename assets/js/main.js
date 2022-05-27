eaef566b25668026d1ba46e7abd7e339c0b02a24
Omar Bautista <joxebus@gmail.com>
2022-05-26 23:47:34 -0500

    Add new section for blogposts from Github

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,7 +27,41 @@ jQuery(document).ready(function($) {
     
     
     /* jQuery RSS - https://github.com/sdepold/jquery-rss */
-    
+
+    $("#rss-feeds-blog-github").rss(
+
+        //Change this to your own rss feeds
+        "https://joxebus.github.io/blog/feed.xml",
+
+        {
+            // how many entries do you want?
+            // default: 4
+            // valid values: any integer
+            limit: 3,
+
+            // the effect, which is used to let the entries appear
+            // default: 'show'
+            // valid values: 'show', 'slide', 'slideFast', 'slideSynced', 'slideFastSynced'
+            effect: 'slideFastSynced',
+
+            // will request the API via https
+            // default: false
+            // valid values: false, true
+            ssl: true,
+
+            // outer template for the html transformation
+            // default: "<ul>{entries}</ul>"
+            // valid values: any string
+            layoutTemplate: "<div class='items'>{entries}</div>",
+
+            // inner template for each entry
+            // default: '<li><a href="{url}">[{author}@{date}] {title}</a><br/>{shortBodyPlain}</li>'
+            // valid values: any string
+            entryTemplate: '<div class="item"><h3 class="title"><a href="{url}" target="_blank">{title}</a></h3><div><p>{shortBodyPlain}</p><a class="more-link" href="{url}" target="_blank"><i class="fas fa-external-link-alt"></i>Read more</a></div></div>'
+
+        }
+    );
+
     $("#rss-feeds-blog").rss(
     
         //Change this to your own rss feeds
