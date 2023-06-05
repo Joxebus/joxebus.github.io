add65237602f23ab1affe5e0fa291ac48fd660ba
Omar Bautista <joxebus@gmail.com>
2023-06-05 16:48:45 -0600

    Replace old assets and fix index page.

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,6 +10,58 @@ $(window).on('load', function() {
         
     });
 
+    new RSS('https://www.youtube.com/feeds/videos.xml?channel_id=UCZlRK9pPpcFBZSNpKlaOXqg',
+            "latest-youtube-video",
+            "item",
+            new EntryOptions(
+                1,
+                '<div class="embed-responsive embed-responsive-16by9 has-ribbon">{entries}</div>',
+                '<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/{guid}" allow="encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe><div class="ribbon"><div class="text">New</div></div>',
+                true
+
+            )).renderEntries()
+
+    new RSS('https://joxebus.github.io/blog/feed.xml',
+            "rss-feeds-blog-github",
+            "item",
+            new EntryOptions(
+                3,
+                "<div class='items'>{entries}</div>",
+                '<div class="item"><h3 class="title"><a href="{link}" target="_blank">{title}</a></h3><div><a class="more-link" href="{link}" target="_blank"><i class="fas fa-external-link-alt"></i>Read more</a></div></div>'
+
+            )).renderEntries()
+
+    new RSS('https://groovylopeando.wordpress.com/feed/',
+            "rss-feeds-blog",
+            "item",
+            new EntryOptions(
+                3,
+                "<div class='items'>{entries}</div>",
+                '<div class="item"><h3 class="title"><a href="{link}" target="_blank">{title}</a></h3><div><p>{description}</p><a class="more-link" href="{link}" target="_blank"><i class="fas fa-external-link-alt"></i>Read more</a></div></div>'
+
+            )).renderEntries()
+
+    new RSS('https://medium.com/feed/@joxebus',
+            "rss-feeds-medium",
+            "item",
+            new EntryOptions(
+                3,
+                "<div class='items'>{entries}</div>",
+                '<div class="item"><h3 class="title"><a href="{link}" target="_blank">{title}</a></h3><div><a class="more-link" href="{link}" target="_blank"><i class="fas fa-external-link-alt"></i>Read more</a></div></div>'
+
+            )).renderEntries()
+
+    new RSS('https://www.youtube.com/feeds/videos.xml?channel_id=UCZlRK9pPpcFBZSNpKlaOXqg',
+            "rss-feeds-youtube",
+            "item",
+            new EntryOptions(
+                5,
+                "<div class='items'>{entries}</div>",
+                '<div class="item"><h3 class="title"><a href="{link}" target="_blank">{title}</a></h3><div><p><img class="img-fluid" src="{thumbnail}"/></p><a class="more-link" href="{link}" target="_blank"><i class="fas fa-external-link-alt"></i>See video</a></div></div>'
+
+            )).renderEntries()
+
+
 });
 
 
@@ -28,155 +80,9 @@ jQuery(document).ready(function($) {
     
     /* jQuery RSS - https://github.com/sdepold/jquery-rss */
 
-    $("#rss-feeds-blog-github").rss(
-
-        //Change this to your own rss feeds
-        "https://joxebus.github.io/blog/feed.xml",
-
-        {
-            // how many entries do you want?
-            // default: 4
-            // valid values: any integer
-            limit: 3,
-
-            // the effect, which is used to let the entries appear
-            // default: 'show'
-            // valid values: 'show', 'slide', 'slideFast', 'slideSynced', 'slideFastSynced'
-            effect: 'slideFastSynced',
-
-            // will request the API via https
-            // default: false
-            // valid values: false, true
-            ssl: true,
-
-            // outer template for the html transformation
-            // default: "<ul>{entries}</ul>"
-            // valid values: any string
-            layoutTemplate: "<div class='items'>{entries}</div>",
-
-            // inner template for each entry
-            // default: '<li><a href="{url}">[{author}@{date}] {title}</a><br/>{shortBodyPlain}</li>'
-            // valid values: any string
-            entryTemplate: '<div class="item"><h3 class="title"><a href="{url}" target="_blank">{title}</a></h3><div><p>{shortBodyPlain}</p><a class="more-link" href="{url}" target="_blank"><i class="fas fa-external-link-alt"></i>Read more</a></div></div>'
-
-        }
-    );
-
-    $("#rss-feeds-blog").rss(
-    
-        //Change this to your own rss feeds
-        "https://groovylopeando.wordpress.com/feed/",
-        
-        {
-        // how many entries do you want?
-        // default: 4
-        // valid values: any integer
-        limit: 3,
-        
-        // the effect, which is used to let the entries appear
-        // default: 'show'
-        // valid values: 'show', 'slide', 'slideFast', 'slideSynced', 'slideFastSynced'
-        effect: 'slideFastSynced',
-        
-        // will request the API via https
-	    // default: false
-	    // valid values: false, true
-	    ssl: true,
-        
-        // outer template for the html transformation
-        // default: "<ul>{entries}</ul>"
-        // valid values: any string
-        layoutTemplate: "<div class='items'>{entries}</div>",
-        
-        // inner template for each entry
-        // default: '<li><a href="{url}">[{author}@{date}] {title}</a><br/>{shortBodyPlain}</li>'
-        // valid values: any string
-        entryTemplate: '<div class="item"><h3 class="title"><a href="{url}" target="_blank">{title}</a></h3><div><p>{shortBodyPlain}</p><a class="more-link" href="{url}" target="_blank"><i class="fas fa-external-link-alt"></i>Read more</a></div></div>'
-        
-        }
-    );
-
-    $("#rss-feeds-medium").rss(
-
-        //Change this to your own rss feeds
-        "https://medium.com/feed/@joxebus",
-
-        {
-        // how many entries do you want?
-        // default: 4
-        // valid values: any integer
-        limit: 3,
-
-        // the effect, which is used to let the entries appear
-        // default: 'show'
-        // valid values: 'show', 'slide', 'slideFast', 'slideSynced', 'slideFastSynced'
-        effect: 'slideFastSynced',
-
-        // will request the API via https
-	    // default: false
-	    // valid values: false, true
-	    ssl: true,
-
-        // outer template for the html transformation
-        // default: "<ul>{entries}</ul>"
-        // valid values: any string
-        layoutTemplate: "<div class='items'>{entries}</div>",
-
-        // inner template for each entry
-        // default: '<li><a href="{url}">[{author}@{date}] {title}</a><br/>{shortBodyPlain}</li>'
-        // valid values: any string
-        entryTemplate: '<div class="item"><h3 class="title"><a href="{url}" target="_blank">{title}</a></h3><div><p>{shortBodyPlain}</p><a class="more-link" href="{url}" target="_blank"><i class="fas fa-external-link-alt"></i>Read more</a></div></div>'
-
-        }
-    );
-
-    $("#latest-youtube-video").rss(
-        "https://www.youtube.com/feeds/videos.xml?channel_id=UCZlRK9pPpcFBZSNpKlaOXqg",
-        {
-            limit: 1,
-            ssl: true,
-            layoutTemplate: '<div class="embed-responsive embed-responsive-16by9 has-ribbon">{entries}</div>',
-            entryTemplate: '<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/{url}" allowfullscreen></iframe><div class="ribbon"><div class="text">New</div></div>'
-        }
-    );
-
-    $("#rss-feeds-youtube").rss(
-
-        //Change this to your own rss feeds
-        "https://www.youtube.com/feeds/videos.xml?channel_id=UCZlRK9pPpcFBZSNpKlaOXqg",
-
-        {
-            // how many entries do you want?
-            // default: 4
-            // valid values: any integer
-            limit: 10,
-
-            // the effect, which is used to let the entries appear
-            // default: 'show'
-            // valid values: 'show', 'slide', 'slideFast', 'slideSynced', 'slideFastSynced'
-            effect: 'slideFastSynced',
-
-            // will request the API via https
-            // default: false
-            // valid values: false, true
-            ssl: true,
-
-            // outer template for the html transformation
-            // default: "<ul>{entries}</ul>"
-            // valid values: any string
-            layoutTemplate: "<div class='items'>{entries}</div>",
-
-            // inner template for each entry
-            // default: '<li><a href="{url}">[{author}@{date}] {title}</a><br/>{shortBodyPlain}</li>'
-            // valid values: any string
-            entryTemplate: '<div class="item"><h3 class="title"><a href="{url}" target="_blank">{title}</a></h3><div><p>{body}</p><a class="more-link" href="{url}" target="_blank"><i class="fas fa-external-link-alt"></i>See video</a></div></div>'
-
-        }
-    );
-    
     
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
-    GitHubActivity.feed({ username: "Joxebus", selector: "#ghfeed" });
+    GitHubActivity.feed({username: "Joxebus", selector: "#ghfeed", limit: 20 });
 
 
 });
